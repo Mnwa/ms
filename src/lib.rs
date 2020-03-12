90cf99c9f7deea7d60b94c6c4d39099c1b0b0bcd
Mnwa <mihan@panfilov.biz>
2020-03-12 12:49:32 +0300

    Remove any dependencies and increase speed from 436ns/iter to 34 ns/iter

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,6 @@
 #![feature(test)]
 extern crate test;
 
-#[macro_use]
-extern crate lazy_static;
-
-use regex::{Captures, Regex};
 use std::fmt::Formatter;
 use std::str::FromStr;
 
@@ -14,10 +10,6 @@ const HOUR: f64 = MINUTE * 60_f64;
 const DAY: f64 = HOUR * 24_f64;
 const WEEK: f64 = DAY * 7_f64;
 const YEAR: f64 = DAY * 365.25_f64;
-
-lazy_static! {
-    static ref REG: Regex = Regex::new(r"^(-?(?:\d+)?\.?\d+) *(milliseconds?|msecs?|ms|seconds?|secs?|s|minutes?|mins?|m|hours?|hrs?|h|days?|d|weeks?|w|years?|yrs?|y)?$").unwrap();
-}
 
 /// Convert time string to a milliseconds
 ///
@@ -29,57 +21,52 @@ lazy_static! {
 /// assert_eq!(value, 86400000.)
 /// ```
 pub fn ms(str: &str) -> Result<f64, Error> {
-    let captures: Captures = REG
-        .captures(str)
-        .map_or(Err(Error::new("fail to parse input")), |captures| {
-            Ok(captures)
-        })?;
-
-    let postfix = captures
-        .get(2)
-        .map_or("ms", |m| m.as_str())
-        .parse::<DatePostfixes>()?;
-    let value: f64 = captures
-        .get(1)
-        .map(|v| v.as_str())
-        .and_then(|v| v.parse::<f64>().ok())
-        .map_or(Err(Error::new("fail to parse value")), |v| Ok(v))?;
+    let postfix = str.parse::<DatePostfixes>()?;
 
     Ok(match postfix {
-        DatePostfixes::Year => value * YEAR,
-        DatePostfixes::Week => value * WEEK,
-        DatePostfixes::Day => value * DAY,
-        DatePostfixes::Hour => value * HOUR,
-        DatePostfixes::Minute => value * MINUTE,
-        DatePostfixes::Second => value * SECOND,
-        DatePostfixes::Millisecond => value,
+        DatePostfixes::Year(v) => v * YEAR,
+        DatePostfixes::Week(v) => v * WEEK,
+        DatePostfixes::Day(v) => v * DAY,
+        DatePostfixes::Hour(v) => v * HOUR,
+        DatePostfixes::Minute(v) => v * MINUTE,
+        DatePostfixes::Second(v) => v * SECOND,
+        DatePostfixes::Millisecond(v) => v,
     })
 }
 
 #[derive(Debug)]
 enum DatePostfixes {
-    Year,
-    Week,
-    Day,
-    Hour,
-    Minute,
-    Second,
-    Millisecond,
+    Year(f64),
+    Week(f64),
+    Day(f64),
+    Hour(f64),
+    Minute(f64),
+    Second(f64),
+    Millisecond(f64),
 }
 
 impl FromStr for DatePostfixes {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "years" | "year" | "yrs" | "yr" | "y" => Ok(DatePostfixes::Year),
-            "weeks" | "week" | "w" => Ok(DatePostfixes::Week),
-            "days" | "day" | "d" => Ok(DatePostfixes::Day),
-            "hours" | "hour" | "hrs" | "hr" | "h" => Ok(DatePostfixes::Hour),
-            "minutes" | "minute" | "mins" | "min" | "m" => Ok(DatePostfixes::Minute),
-            "seconds" | "second" | "secs" | "sec" | "s" => Ok(DatePostfixes::Second),
-            "milliseconds" | "millisecond" | "msecs" | "msec" | "ms" => {
-                Ok(DatePostfixes::Millisecond)
+        let value_count = s
+            .chars()
+            .take_while(|c| c >= &'0' && c <= &'9' || c == &'.' || c == &'-')
+            .count();
+        let (value, postfix) = s.split_at(value_count);
+        let value = value
+            .parse::<f64>()
+            .map_or(Err(Error::new("invalid value")), |v| Ok(v))?;
+
+        match postfix.trim() {
+            "years" | "year" | "yrs" | "yr" | "y" => Ok(DatePostfixes::Year(value)),
+            "weeks" | "week" | "w" => Ok(DatePostfixes::Week(value)),
+            "days" | "day" | "d" => Ok(DatePostfixes::Day(value)),
+            "hours" | "hour" | "hrs" | "hr" | "h" => Ok(DatePostfixes::Hour(value)),
+            "minutes" | "minute" | "mins" | "min" | "m" => Ok(DatePostfixes::Minute(value)),
+            "seconds" | "second" | "secs" | "sec" | "s" => Ok(DatePostfixes::Second(value)),
+            "milliseconds" | "millisecond" | "msecs" | "msec" | "ms" | "" => {
+                Ok(DatePostfixes::Millisecond(value))
             }
             _ => Err(Error::new("invalid postfix")),
         }
